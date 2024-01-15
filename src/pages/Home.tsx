c300558bdd99269390f68f96f7ba5d0a4b81a2af
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-15 10:23:43 +0530

    verify added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,37 +3,37 @@ import { useEffect, useState } from "react";
 import { serverUrl } from "../lib/utils";
 
 function getCookie(key: string) {
-  const b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
-  return b ? b.pop() : "";
+	const b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
+	return b ? b.pop() : "";
 }
 
 export const Home = () => {
-  const [user, setUser] = useState<Record<string, number>>({});
+	const [user, setUser] = useState<Record<string, number>>({});
 
-  useEffect(() => {
-    const cookie = getCookie("token");
-    async function verifyAuth() {
-      const { data } = await axios.post(
-        serverUrl,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${cookie}`,
-          },
-          withCredentials: true,
-        },
-      );
-      console.log(data);
-      setUser(data.user);
-    }
-    console.log(cookie);
-    verifyAuth();
-  }, []);
+	useEffect(() => {
+		const cookie = getCookie("token");
+		async function verifyAuth() {
+			const { data } = await axios.post(
+				`${serverUrl}/verify`,
+				{},
+				{
+					headers: {
+						Authorization: `Bearer ${cookie}`,
+					},
+					withCredentials: true,
+				},
+			);
+			console.log(data);
+			setUser(data.user);
+		}
+		console.log(cookie);
+		verifyAuth();
+	}, []);
 
-  return (
-    <div>
-      Home
-      <h1>{user?.username ? user.username : "logged out"}</h1>
-    </div>
-  );
+	return (
+		<div>
+			Home
+			<h1>{user?.username ? user.username : "logged out"}</h1>
+		</div>
+	);
 };
