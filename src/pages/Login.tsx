ce624541212ffd53a226fdf19c44232232a88840
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-15 10:51:02 +0530

    / removed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,109 +6,110 @@ import { Link } from "react-router-dom";
 import { serverUrl } from "../lib/utils";
 
 type InputValue = {
-  username: string;
-  password: string;
+	username: string;
+	password: string;
 };
 
 export const Login = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [inputValue, setInputValue] = useState<InputValue>({
-    username: "",
-    password: "",
-  });
+	const [isLoading, setIsLoading] = useState(false);
+	const [inputValue, setInputValue] = useState<InputValue>({
+		username: "",
+		password: "",
+	});
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    console.log(inputValue);
-    try {
-      setIsLoading(true);
-      const response = await axios.post(
-        `${serverUrl}/login`,
-        {
-          ...inputValue,
-        },
-        { withCredentials: true },
-      );
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
 
-  return (
-    <Container
-      fluid
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <h1>Login</h1>
-      <Form style={{ width: "50%", height: "50%" }} onSubmit={handleSubmit}>
-        <Form.Group
-          className="mb-3"
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            gap: "10px",
-          }}
-          controlId="formBasicEmail"
-        >
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            style={{ width: "600px" }}
-            type="text"
-            placeholder="Username"
-            name="username"
-            value={inputValue.username}
-            onChange={(e) =>
-              setInputValue((prev) => ({
-                ...prev,
-                [e.target.name]: e.target.value,
-              }))
-            }
-          />
-        </Form.Group>
+		console.log(serverUrl);
+		try {
+			setIsLoading(true);
+			const response = await axios.post(
+				`${serverUrl}login`,
+				{
+					...inputValue,
+				},
+				{ withCredentials: true },
+			);
+			console.log(response);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-        <Form.Group
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            gap: "10px",
-          }}
-          className="mb-3"
-          controlId="formBasicPassword"
-        >
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            style={{ width: "600px" }}
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={inputValue.password}
-            onChange={(e) =>
-              setInputValue((prev) => ({
-                ...prev,
-                [e.target.name]: e.target.value,
-              }))
-            }
-          />
-        </Form.Group>
-        <div style={{ display: "flex", gap: "10px" }}>
-          <h1>New User?</h1>
-          <Link to="/signin">SignIn</Link>
-        </div>
-        <Button variant="primary" type="submit">
-          {isLoading ? "Loading..." : "Log In"}
-        </Button>
-      </Form>
-    </Container>
-  );
+	return (
+		<Container
+			fluid
+			style={{
+				height: "100vh",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				flexDirection: "column",
+			}}
+		>
+			<h1>Login</h1>
+			<Form style={{ width: "50%", height: "50%" }} onSubmit={handleSubmit}>
+				<Form.Group
+					className="mb-3"
+					style={{
+						display: "flex",
+						justifyContent: "space-between",
+						alignItems: "center",
+						gap: "10px",
+					}}
+					controlId="formBasicEmail"
+				>
+					<Form.Label>Username</Form.Label>
+					<Form.Control
+						style={{ width: "600px" }}
+						type="text"
+						placeholder="Username"
+						name="username"
+						value={inputValue.username}
+						onChange={(e) =>
+							setInputValue((prev) => ({
+								...prev,
+								[e.target.name]: e.target.value,
+							}))
+						}
+					/>
+				</Form.Group>
+
+				<Form.Group
+					style={{
+						display: "flex",
+						justifyContent: "space-between",
+						alignItems: "center",
+						gap: "10px",
+					}}
+					className="mb-3"
+					controlId="formBasicPassword"
+				>
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						style={{ width: "600px" }}
+						type="password"
+						placeholder="Password"
+						name="password"
+						value={inputValue.password}
+						onChange={(e) =>
+							setInputValue((prev) => ({
+								...prev,
+								[e.target.name]: e.target.value,
+							}))
+						}
+					/>
+				</Form.Group>
+				<div style={{ display: "flex", gap: "10px" }}>
+					<h1>New User?</h1>
+					<Link to="/signin">SignIn</Link>
+				</div>
+				<Button variant="primary" type="submit">
+					{isLoading ? "Loading..." : "Log In"}
+				</Button>
+			</Form>
+		</Container>
+	);
 };
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -6,109 +6,109 @@ import { Link } from "react-router-dom";
 import { serverUrl } from "../lib/utils";
 
 type InputValue = {
-  username: string;
-  password: string;
+	username: string;
+	password: string;
 };
 
 export const Signin = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [inputValue, setInputValue] = useState<InputValue>({
-    username: "",
-    password: "",
-  });
+	const [isLoading, setIsLoading] = useState(false);
+	const [inputValue, setInputValue] = useState<InputValue>({
+		username: "",
+		password: "",
+	});
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-    console.log(inputValue);
-    try {
-      setIsLoading(true);
-      const response = await axios.post(
-        `${serverUrl}/signin`,
-        {
-          ...inputValue,
-        },
-        { withCredentials: true },
-      );
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+	const handleSubmit = async (e: FormEvent) => {
+		e.preventDefault();
+		console.log(inputValue);
+		try {
+			setIsLoading(true);
+			const response = await axios.post(
+				`${serverUrl}signin`,
+				{
+					...inputValue,
+				},
+				{ withCredentials: true },
+			);
+			console.log(response);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-  return (
-    <Container
-      fluid
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <h1>Sign In</h1>
-      <Form style={{ width: "50%", height: "50%" }} onSubmit={handleSubmit}>
-        <Form.Group
-          className="mb-3"
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            gap: "10px",
-          }}
-          controlId="formBasicEmail"
-        >
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            style={{ width: "600px" }}
-            type="text"
-            placeholder="Username"
-            name="username"
-            value={inputValue.username}
-            onChange={(e) =>
-              setInputValue((prev) => ({
-                ...prev,
-                [e.target.name]: e.target.value,
-              }))
-            }
-          />
-        </Form.Group>
+	return (
+		<Container
+			fluid
+			style={{
+				height: "100vh",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				flexDirection: "column",
+			}}
+		>
+			<h1>Sign In</h1>
+			<Form style={{ width: "50%", height: "50%" }} onSubmit={handleSubmit}>
+				<Form.Group
+					className="mb-3"
+					style={{
+						display: "flex",
+						justifyContent: "space-between",
+						alignItems: "center",
+						gap: "10px",
+					}}
+					controlId="formBasicEmail"
+				>
+					<Form.Label>Username</Form.Label>
+					<Form.Control
+						style={{ width: "600px" }}
+						type="text"
+						placeholder="Username"
+						name="username"
+						value={inputValue.username}
+						onChange={(e) =>
+							setInputValue((prev) => ({
+								...prev,
+								[e.target.name]: e.target.value,
+							}))
+						}
+					/>
+				</Form.Group>
 
-        <Form.Group
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            gap: "10px",
-          }}
-          className="mb-3"
-          controlId="formBasicPassword"
-        >
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            style={{ width: "600px" }}
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={inputValue.password}
-            onChange={(e) =>
-              setInputValue((prev) => ({
-                ...prev,
-                [e.target.name]: e.target.value,
-              }))
-            }
-          />
-        </Form.Group>
-        <div style={{ display: "flex", gap: "10px" }}>
-          <h1>Already have an Account?</h1>
-          <Link to="/login">Log In</Link>
-        </div>
-        <Button variant="primary" type="submit">
-          {isLoading ? "Loading..." : "Sign In"}
-        </Button>
-      </Form>
-    </Container>
-  );
+				<Form.Group
+					style={{
+						display: "flex",
+						justifyContent: "space-between",
+						alignItems: "center",
+						gap: "10px",
+					}}
+					className="mb-3"
+					controlId="formBasicPassword"
+				>
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						style={{ width: "600px" }}
+						type="password"
+						placeholder="Password"
+						name="password"
+						value={inputValue.password}
+						onChange={(e) =>
+							setInputValue((prev) => ({
+								...prev,
+								[e.target.name]: e.target.value,
+							}))
+						}
+					/>
+				</Form.Group>
+				<div style={{ display: "flex", gap: "10px" }}>
+					<h1>Already have an Account?</h1>
+					<Link to="/login">Log In</Link>
+				</div>
+				<Button variant="primary" type="submit">
+					{isLoading ? "Loading..." : "Sign In"}
+				</Button>
+			</Form>
+		</Container>
+	);
 };
